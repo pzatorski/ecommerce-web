164cfe5d417b28d9e913d0445d069d30b0bcccb2
Patryk Zatorski <patzat95@gmail.com>
2018-01-29 13:56:33 +0000

    start landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import LandingPage from './landing-page';
+import Basket from './basket';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>Hello world</p>
-      </div>
+      <Router>
+        <div>
+          <Route exact path={'/'} component={LandingPage} />
+          <Route exact path={'/basket'} component={Basket} />
+        </div>
+      </Router>
     );
   }
 }
